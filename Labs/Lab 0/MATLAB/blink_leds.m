da88be2c3fec86aec1a6e4c3f70d39bb12f5e467
Damein <Morgan>
2013-01-04 11:46:37 -0800

    Fixed directory structure and added supporting files for lab 0

---
+++ Labs/Lab 0/MATLAB/blink_leds.m
@@ -0,0 +1,16 @@
+function leds_out = blink_leds(num_leds_pow2_in, cycles_per_count_in)
+    persistent value count
+    if isempty(value)
+        count = 0;
+        value = 0;
+    end
+    count = count + 1;
+    if count >= cycles_per_count_in
+        count = 0;
+        value = value + 1;
+        if value >= num_leds_pow2_in
+            value = 0;
+        end
+    end
+    leds_out = value;
+end
