9a7ba97d3f3c5dc26da723a9e09a2f742145f0a0
Damein <dmorgan@toyon.com>
2013-11-15 14:54:34 -0800

    MATLAB Files for Lab 0

--- Labs/Lab_0/MATLAB/blink_leds.m
+++ Labs/Lab_0/MATLAB/blink_leds.m
@@ -1,22 +1,15 @@
 %#codegen
-function leds_out = ...
-  blink_leds(num_leds_pow2_in, cycles_per_count_in)
- 
-persistent value count
- 
-if isempty(value)
-    count = 0;
-    value = 0;
-end
- 
-leds_out = value;
- 
-count = count + 1;
- 
-if count >= cycles_per_count_in
-    count = 0;
-    value = value + 1;
-    if value >= num_leds_pow2_in
-        value = 0;
+function [ blinky ] = blink_leds(dummy_input)
+
+    persistent blinky_cnt
+
+    if (isempty(blinky_cnt))
+        blinky_cnt = 0;
     end
+    
+    blinky_cnt = blinky_cnt + 1;
+    if blinky_cnt == 160000000
+        blinky_cnt = 0;
+    end
+    blinky = floor(blinky_cnt/10000000);
 end
