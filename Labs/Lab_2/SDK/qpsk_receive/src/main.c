cca15d21b9281a9b27db133f63bc1bdffdf470a0
Damein <dmorgan@toyon.com>
2013-11-18 14:29:47 -0800

    added new SDK project for Lab 2

---
+++ Labs/Lab_2/SDK/qpsk_receive/src/main.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include "platform.h"
+#include "xbasic_types.h"
+#include "xuartps.h"
+
+void mcu_latch_registers (void);
+int Chilipepper_InitDone( void );
+
+// mcu registers
+Xuint32 chili_init_done, chili_pa_en, chili_tr_sw,
+                chili_mcu_reset, chili_rx_en, chili_tx_en;
+Xuint32 chili_mcu_latch, chili_mcu_reg_reset,
+                chili_latch_done, chili_reg_reset_done;
+
+// DAC registers
+Xuint32 dac_en;
+
+int main()
+{
+    init_platform();
+
+    // MCU core
+	chili_mcu_reset =             XPAR_MCU_DRIVER_S_AXI_BASEADDR + 0x100; // write
+	chili_tr_sw =                 XPAR_MCU_DRIVER_S_AXI_BASEADDR + 0x104; // write
+	chili_pa_en =                 XPAR_MCU_DRIVER_S_AXI_BASEADDR + 0x108; // write
+	chili_tx_en =                 XPAR_MCU_DRIVER_S_AXI_BASEADDR + 0x10C; // write
+	chili_rx_en =                 XPAR_MCU_DRIVER_S_AXI_BASEADDR + 0x110; // write
+    chili_mcu_latch =             XPAR_MCU_DRIVER_S_AXI_BASEADDR + 0x114; // write
+    chili_mcu_reg_reset =         XPAR_MCU_DRIVER_S_AXI_BASEADDR + 0x118; // write
+
+    chili_init_done =             XPAR_MCU_DRIVER_S_AXI_BASEADDR + 0x11C; // read
+    chili_latch_done =            XPAR_MCU_DRIVER_S_AXI_BASEADDR + 0x120; // read
+    chili_reg_reset_done =        XPAR_MCU_DRIVER_S_AXI_BASEADDR + 0x124; // read
+
+    // Initialization
+    Xil_Out32(chili_tr_sw, 1);
+    Xil_Out32(chili_pa_en, 0);
+
+    //enable physical DAC and ADC
+    Xil_Out32(chili_tx_en, 1);
+    Xil_Out32(chili_rx_en, 1);
+
+	// reset Chilipepper
+	Xil_Out32(chili_mcu_reset, 1);
+	mcu_latch_registers();
+	Xil_Out32(chili_mcu_reset, 0);
+	mcu_latch_registers();
+	Xil_Out32(chili_mcu_reset, 1);
+	mcu_latch_registers();
+
+	// don't continue until we get a valid init back from Chilipepper.
+	while (Chilipepper_InitDone() == 0)
+			;
+
+    while (1);
+
+    cleanup_platform();
+
+    return 0;
+}
+
+void mcu_latch_registers (void){
+        Xil_Out32(chili_mcu_latch, 1);
+        while(chili_latch_done == 0)
+                ;
+        Xil_Out32(chili_mcu_latch, 0);
+        while(chili_latch_done == 1)
+                ;
+}
+
+int Chilipepper_InitDone( void )
+{
+        int done;
+        done = Xil_In32(chili_init_done);
+        return done;
+}
