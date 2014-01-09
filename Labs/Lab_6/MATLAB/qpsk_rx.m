cb2fb8b24e6f2ab2051bba94ecc3e45aac13bad8
Damein <Morgan>
2014-01-08 17:12:55 -0800

    slight update to Lab 6 MATLAB files

--- Labs/Lab_6/MATLAB/qpsk_rx.m
+++ Labs/Lab_6/MATLAB/qpsk_rx.m
@@ -13,39 +13,28 @@
 % tart of packet.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %#codegen
-
-function [r_out, s_t_i, s_t_q, t_est_out, blinky] = ...
+function [s_t_i, s_t_q, blinky, tauh] =...
     qpsk_rx(i_in, q_in)
 
-    persistent blinky_cnt
-    
-    if isempty(blinky_cnt)
-        blinky_cnt = 0;
-    end
+persistent blinky_cnt
 
-    % scale input data coming from the Chilipepper ADC to be purely fractional
-    % to avoid scaling issues
-    r_in = complex(i_in, q_in);
-
-    % frequency offset estimation. Note that time constant is input as  a 
-    % constant integer with value 40
-    [s_f_i, s_f_q] = qpsk_rx_foc(i_in, q_in, 40);
-    
-    % Square-root raised-cosine band-limited filtering
-    [s_c_i, s_c_q] = qpsk_rx_srrc(s_f_i, s_f_q);
-
-    % Time offset estimation. Output data changes at the symbol rate.
-    [s_t_i, s_t_q, tauh] = qpsk_rx_toc(s_c_i, s_c_q, 327);
-
-    % estimation value for frequency offset
-    t_est_out = tauh;
-
-    % original signal out (real version)
-    r_out = real(r_in);
-
-    blinky_cnt = blinky_cnt + 1;
-    if blinky_cnt == 20000000
-        blinky_cnt = 0;
-    end
-    blinky = floor(blinky_cnt/10000000);
+if isempty(blinky_cnt)
+    blinky_cnt = 0;
 end
+
+% frequency offset estimation. Note that time constant is input as integer
+[s_f_i, s_f_q] = ...
+    qpsk_rx_foc(i_in, q_in);
+
+% Square-root raised-cosine band-limited filtering
+[s_c_i, s_c_q] = qpsk_rx_srrc(s_f_i, s_f_q);
+
+% Time offset estimation. Output data changes at the symbol rate.
+[s_t_i, s_t_q, tauh] = ...
+    qpsk_rx_toc(s_c_i, s_c_q);
+
+blinky_cnt = blinky_cnt + 1;
+if blinky_cnt == 20000000
+    blinky_cnt = 0;
+end
+blinky = floor(blinky_cnt/10000000);
--- Labs/Lab_6/MATLAB/qpsk_tb.m
+++ Labs/Lab_6/MATLAB/qpsk_tb.m
@@ -1,125 +1,152 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Model/simulation parameters
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-sim=1;
 OS_RATE = 8;
+SNR = 100;
 fc = 10e3/20e6; % sample rate is 20 MHz, top is 10 kHz offset
-muFOC = floor(.01*2^12)/2^12; % = 40 (if changing this variale, 
-% change the hardcoded value in qpsk_rx as well)
+sim = 1;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Initialize LUTs
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 make_srrc_lut;
 make_trig_lut;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-if (sim)
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % Emulate microprocessor packet creation
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % data payload creation
-    messageASCII = 'hello world!';
-    message = double(unicode2native(messageASCII));
-    % add on length of message to the front with four bytes
-    msgLength = length(message);
-    messageWithNumBytes =[ ...
-        mod(msgLength,2^8) ...
-        mod(floor(msgLength/2^8),2^8) ...
-        mod(floor(msgLength/2^16),2^8) ...
-        1 ... % message ID
-        message];
-    % add two bytes at the end, which is a CRC
-    messageWithCRC = CreateAppend16BitCRC(messageWithNumBytes);
-    ml = length(messageWithCRC);
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % FPGA radio transmit core
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    data_in = 0;
-    empty_in = 1;
-    tx_en_in = 0;
-    store_byte = 0;
-    numBytesFromFifo = 0;
-    num_samp = ml*8*2*2*3;
-    x = zeros(1,num_samp);
-    CORE_LATENCY = 4;
-    data_buf = zeros(1,CORE_LATENCY);
-    store_byte_buf = zeros(1,CORE_LATENCY);
-    clear_buf = zeros(1,CORE_LATENCY);
-    tx_en_buf = zeros(1,CORE_LATENCY);
-    re_byte_out(1) = 0;
-    reset_fifo = 0;
-    byte_request = 0;
-    for i1 = 1:num_samp
-        % first thing the processor does is clear the internal tx fifo
-        if i1 == 1
-            clear_fifo_in = 1;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Emulate microprocessor packet creation
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% data payload creation
+messageASCII = 'hello world!';
+message = double(unicode2native(messageASCII));
+% add on length of message to the front with four bytes
+msgLength = length(message);
+messageWithNumBytes =[ ...
+    mod(msgLength,2^8) ...
+    mod(floor(msgLength/2^8),2^8) ...
+    mod(floor(msgLength/2^16),2^8) ...
+    1 ... % message ID
+    message];
+% add two bytes at the end, which is a CRC
+messageWithCRC = CreateAppend16BitCRC(messageWithNumBytes);
+ml = length(messageWithCRC);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% FPGA radio transmit core
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+data_in = 0;
+empty_in = 1;
+tx_en_in = 0;
+store_byte = 0;
+numBytesFromFifo = 0;
+num_samp = ml*8*2*2*3;
+x = zeros(1,num_samp);
+CORE_LATENCY = 4;
+data_buf = zeros(1,CORE_LATENCY);
+store_byte_buf = zeros(1,CORE_LATENCY);
+clear_buf = zeros(1,CORE_LATENCY);
+tx_en_buf = zeros(1,CORE_LATENCY);
+re_byte_out(1) = 0;
+reset_fifo = 0;
+byte_request = 0;
+for i1 = 1:num_samp
+    % first thing the processor does is clear the internal tx fifo
+    if i1 == 1
+        clear_fifo_in = 1;
+    else
+        clear_fifo_in = 0;
+    end
+
+    data_buf = [data_buf(2:end) data_in];
+    store_byte_buf = [store_byte_buf(2:end) store_byte];
+    clear_buf = [clear_buf(2:end) clear_fifo_in];
+    tx_en_buf = [tx_en_buf(2:end) tx_en_in];
+    
+    [new_data_in, empty_in, byte_recieved, full, percent_full] = ...
+    tx_fifo(byte_request, store_byte_buf(1), data_buf(1), reset_fifo);
+    
+    [i_out, q_out, tx_done_out, request_byte, clear_fifo_in_done] = ...
+        qpsk_tx(new_data_in,empty_in,clear_buf(1),tx_en_buf(1));
+    x_out = complex(i_out,q_out)/2^11;
+    x(i1) = x_out;
+    byte_request = request_byte;
+    
+    %%% Emulate write to FIFO interface
+    if mod(i1,8) == 1 && numBytesFromFifo < length(messageWithCRC)
+        data_in = messageWithCRC(numBytesFromFifo+1);
+        numBytesFromFifo = numBytesFromFifo + 1;
+    end
+    %%% Software lags a but on the handshaking signals %%%
+    if (0 < mod(i1,8) && mod(i1,8) < 5) && tx_en_in == 0
+        store_byte = 1;
+    else
+        store_byte = 0;
+    end
+    % processor loaded all bytes into FIFO so begin transmitting
+    if (numBytesFromFifo == length(messageWithCRC) && mod(i1,8) > 5)
+        empty_in = 1;
+        tx_en_in = 1;
+    end
+end
+if ~sim % load data that was transmitted and captured from chipscope
+    if 1
+        fid = fopen('tx.prn');
+        M = textscan(fid,'%d %d %d %d %d %d %d %d %d %d','Headerlines',1);
+        fclose(fid);
+        iFile = double(M{3})'/2^11;
+        qFile = double(M{4})'/2^11;
+    else
+        M = load('dac.prn');
+        if M(1,end-1) == 0
+            iFile = M(1:2:end,end)'/2^11;
+            qFile = M(2:2:end,end)'/2^11;
         else
-            clear_fifo_in = 0;
-        end
-
-        data_buf = [data_buf(2:end) data_in];
-        store_byte_buf = [store_byte_buf(2:end) store_byte];
-        clear_buf = [clear_buf(2:end) clear_fifo_in];
-        tx_en_buf = [tx_en_buf(2:end) tx_en_in];
-
-        [new_data_in, empty_in, byte_recieved, full, percent_full] = ...
-        tx_fifo(byte_request, store_byte_buf(1), data_buf(1), reset_fifo);
-
-        [i_out, q_out, tx_done_out, request_byte] = ...
-            qpsk_tx(new_data_in,empty_in,clear_buf(1),tx_en_buf(1));
-        x_out = complex(i_out,q_out)/2^11;
-        x(i1) = x_out;
-        byte_request = request_byte;
-
-        %%% Emulate write to FIFO interface
-        if mod(i1,8) == 1 && numBytesFromFifo < length(messageWithCRC)
-            data_in = messageWithCRC(numBytesFromFifo+1);
-            numBytesFromFifo = numBytesFromFifo + 1;
-        end
-        %%% Software lags a bit on the handshaking signals %%%
-        if (0 < mod(i1,8) && mod(i1,8) < 5) && tx_en_in == 0
-            store_byte = 1;
-        else
-            store_byte = 0;
-        end
-        % processor loaded all bytes into FIFO so begin transmitting
-        if (numBytesFromFifo == length(messageWithCRC) && mod(i1,8) > 5)
-            empty_in = 1;
-            tx_en_in = 1;
+            qFile = M(1:2:end,end)'/2^11;
+            iFile = M(2:2:end,end)'/2^11;
         end
     end
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % Emulate channel
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % pad on either side with zeros
-    p = complex(zeros(1,100),zeros(1,100));
-    xp = [p x p]; % pad
-
-    % Apply frequency offset
-    y = xp.*exp(1i*2*pi*fc*(0:length(xp)-1));
-    r = y/max(abs(y))*.1*2^11; % this controls receive gain
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    x = complex(iFile,qFile);
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Load Chipscope samples
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-if (~sim)
+% Emulate channel
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% pad on either side with zeros
+p = complex(zeros(1,100),zeros(1,100));
+xp = [p x p]; % pad
+
+% Apply frequency offset and receive/over-the-air AWGN
+y = xp.*exp(1i*2*pi*fc*(0:length(xp)-1));
+rC = y/max(abs(y))*.1*2^11; % this controls receive gain
+%r = awgn(rC,SNR,0,1);
+r = rC;
+if ~sim
+    %M = load('rx.prn');
     fid = fopen('rx.prn');
+    %M = textscan(fid,'%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d','Headerlines',1);
     M = textscan(fid,'%d %d %d %d','Headerlines',1);
     fclose(fid);
     is = double(M{3});
     qs = double(M{4});
     r = complex(is,qs);
+    figure(1)
+    subplot(2,1,1);
+    plot(is);
+    subplot(2,1,2);
+    plot(qs)
 end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Main receiver core
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 r_out = zeros(1,length(r));
-s_t = zeros(1,length(r));
-t_est = zeros(1,length(r));
+s_t_i = zeros(1,length(r));
+s_t_q = zeros(1,length(r));
+tauh = zeros(1,length(r));
 for i1 = 1:length(r)+200
     if i1 > length(r)
         r_in = 0;
@@ -128,20 +155,27 @@ for i1 = 1:length(r)+200
     end
     i_in = round(real(r_in));
     q_in = round(imag(r_in));
-    [r_out(i1), s_t_i, s_t_q, t_est(i1)] = qpsk_rx(i_in, q_in);
-    s_t(i1) = complex(s_t_i,s_t_q);
+    r_out(i1) = real(complex(i_in,q_in));
+   
+    [dc_i_out, dc_q_out, rssi_out, rssi_en_out, dir_out, dir_en_out] = ...
+        dc_offset_correction(i_in, q_in, mod(i1,2), 500, 1500, +(i1>3000));
+    
+    [s_t_i(i1), s_t_q(i1), blinky, tauh(i1)] =...
+        qpsk_rx(dc_i_out, dc_q_out);
+
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 figure(2)
 subplot(2,2,1)
 scatter(real(r),imag(r))
-title('Pre FOC Signal');
-subplot(2,2,3)
-plot(real(r_out));
-title('Pre FOC Signal (real part)');
+title('OTA Receive Signal');
 subplot(2,2,2)
-scatter(real(s_t),imag(s_t))
+plot(real(r_out));
+title('OTA Receive Signal (real part)');
+subplot(2,2,3)
+scatter(s_t_i,s_t_q)
 title('Signal Post TOC');
 subplot(2,2,4)
-plot(t_est);
+plot(tauh);
 title('Timing estimate');
