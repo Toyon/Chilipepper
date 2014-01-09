01730d3c86323cc55996e7e480369a44940c1196
Damein <Morgan>
2014-01-08 16:58:46 -0800

    slight update to Lab 7 EDK project

--- Labs/Lab_7/EDK/pcores/qpsk_rx_pcore_v1_00_a/include/qpsk_rx_pcore_addr.h
+++ Labs/Lab_7/EDK/pcores/qpsk_rx_pcore_v1_00_a/include/qpsk_rx_pcore_addr.h
@@ -1,15 +1,13 @@
 /*
  * File Name:         codegen\ipcore\qpsk_rx_pcore_v1_00_a\include\qpsk_rx_pcore_addr.h
  * Description:       C Header File
- * Created:           2013-12-13 17:33:31
+ * Created:           2014-01-04 15:01:31
 */
 
 #ifndef QPSK_RX_PCORE_H_
 #define QPSK_RX_PCORE_H_
 
-#define  IPCore_Reset_qpsk_rx_pcore           0x0  //write 0x1 to bit 0 to reset IP core
-#define  IPCore_Enable_qpsk_rx_pcore          0x4  //enabled (by default) when bit 0 is 0x1
-#define  mcu_rx_ready_in_Data_qpsk_rx_pcore   0x100  //data register for port mcu_rx_ready_in
-#define  num_bytes_ready_Data_qpsk_rx_pcore   0x104  //data register for port num_bytes_ready
+#define  IPCore_Reset_qpsk_rx_pcore    0x0  //write 0x1 to bit 0 to reset IP core
+#define  IPCore_Enable_qpsk_rx_pcore   0x4  //enabled (by default) when bit 0 is 0x1
 
 #endif /* QPSK_RX_PCORE_H_ */
