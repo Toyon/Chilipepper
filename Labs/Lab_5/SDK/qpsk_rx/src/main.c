35bc3e26c283b4cdc6f1133b960f3d60773f3b67
Damein <Morgan>
2014-01-08 17:08:34 -0800

    update to Lab 5 SDK Library Files

--- Labs/Lab_5/SDK/qpsk_rx/src/main.c
+++ Labs/Lab_5/SDK/qpsk_rx/src/main.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 #include "platform.h"
-#include "xparameters.h"
 #include "chilipepper.h"
+#include "xuartps.h"
+
+XUartPs uartPs;
+XUartPs_Config *pUartPsConfig;
 
 int main()
 {
+
     init_platform();
 
-	if ( Chilipepper_Initialize() != 0 )
-		return -1;
+    if ( Chilipepper_Initialize() != 0 )
+    	return -1;
 
-	// by default we are in transmit
-	Chilipepper_SetPA( 0 );
-	Chilipepper_SetTxRxSw( 1 ); // 0- transmit, 1-receive
+    Chilipepper_SetPA( 0 );
+    Chilipepper_SetTxRxSw( 1 ); // 0- transmit, 1-receive
+    Chilipepper_SetDCOC(1);		// enable dc offset correction
 
-    while (1){
-  	Chilipepper_ControlAgc(); //update the Chilipepper AGC
+    while (1)
+    {
+    	Chilipepper_ControlAgc(); //update the Chilipepper AGC
     }
-
     cleanup_platform();
-
     return 0;
 }
