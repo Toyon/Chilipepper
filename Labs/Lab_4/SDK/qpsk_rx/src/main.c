1d8d964a3062b454299ade7a8ab5fd15bf2843d7
Damein <Morgan>
2014-01-16 14:01:59 -0800

    update to timing for Lab 4

--- Labs/Lab_4/SDK/qpsk_rx/src/main.c
+++ Labs/Lab_4/SDK/qpsk_rx/src/main.c
@@ -9,19 +9,19 @@ XUartPs_Config *pUartPsConfig;
 int main()
 {
 
-    init_platform();
+	init_platform();
 
-    if ( Chilipepper_Initialize() != 0 )
-    	return -1;
+	if ( Chilipepper_Initialize() != 0 )
+		return -1;
 
-    Chilipepper_SetPA( 0 );
-    Chilipepper_SetTxRxSw( 1 ); // 0- transmit, 1-receive
-    Chilipepper_SetDCOC(1);		// enable dc offset correction
+	Chilipepper_SetPA( 0 );
+	Chilipepper_SetTxRxSw( 1 ); // 0- transmit, 1-receive
+	Chilipepper_SetDCOC(1);		// enable dc offset correction
 
-    while (1)
-    {
-    	Chilipepper_ControlAgc(); //update the Chilipepper AGC
-    }
-    cleanup_platform();
-    return 0;
+	while (1)
+	{
+		Chilipepper_ControlAgc(); //update the Chilipepper AGC
+	}
+	cleanup_platform();
+	return 0;
 }
