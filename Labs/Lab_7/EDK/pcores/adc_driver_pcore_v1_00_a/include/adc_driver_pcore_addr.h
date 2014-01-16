f4c9fa5032f536264bd7fc0575a2092569861924
Damein <Morgan>
2014-01-16 15:41:17 -0800

    update to Lab 7 timing

--- Labs/Lab_7/EDK/pcores/adc_driver_pcore_v1_00_a/include/adc_driver_pcore_addr.h
+++ Labs/Lab_7/EDK/pcores/adc_driver_pcore_v1_00_a/include/adc_driver_pcore_addr.h
@@ -1,7 +1,7 @@
 /*
  * File Name:         codegen\ipcore\adc_driver_pcore_v1_00_a\include\adc_driver_pcore_addr.h
  * Description:       C Header File
- * Created:           2013-12-30 15:48:14
+ * Created:           2014-01-16 12:23:10
 */
 
 #ifndef ADC_DRIVER_PCORE_H_
