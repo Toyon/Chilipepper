60788ac29a91cfc7b574fce6588c9a1a630c1c36
Damein <Morgan>
2014-01-16 16:55:18 -0800

    slight update to Lab 9 MATLAB files

--- Labs/Lab_9/MATLAB/tx_fifo.m
+++ Labs/Lab_9/MATLAB/tx_fifo.m
@@ -1,67 +1,67 @@
-function [dout, empty, byte_recieved, full, bytes_available] = ...
-    tx_fifo(get_byte, store_byte, byte_in, reset_fifo)
-%
-%  First In First Out (FIFO) structure.
-%  This FIFO stores integers.
-%  The FIFO is actually a circular buffer.
-%
-persistent head tail fifo byte_out debounce
+function [dout, bytes_available, byte_received, empty] = ...
+    tx_fifo(reset_fifo, store_byte, byte_in, get_byte)
+    %
+    %  First In First Out (FIFO) structure.
+    %  This FIFO stores integers.
+    %  The FIFO is actually a circular buffer.
+    %
+    persistent head tail fifo byte_out handshake
 
-if (isempty(head) || reset_fifo == 1)
-    head = 1;
-    tail = 2;
-    byte_out = 0;
-    debounce = 0;
-end
-
-if isempty(fifo)
-    fifo = zeros(1,1024);
-end
-
-full = 0;
-empty = 0;
-byte_recieved = 0;
-
-if ((tail == 1 && head == 1024) || ((head + 1) == tail))
-    empty = 1;
-end
-if ((head == 1 && tail == 1024) || ((tail + 1) == head))
-    full = 1;
-end
-if store_byte == 0
-    debounce = 0;
-end
-if debounce == 1
-    byte_recieved = 1;
-end
-
-%%%%%%%%%%%%%%get%%%%%%%%%%%%%%%%%%%%%
-if (get_byte && empty == 0)
-    head = head + 1;
-    if head == 1025
+    if (reset_fifo || isempty(head))
         head = 1;
+        tail = 2;
+        byte_out = 0;
+        handshake = 0;
     end
-    byte_out = fifo(head);
-end
 
-%%%%%%%%%%%%%put%%%%%%%%%%%%%%%%%%%%%
-if (store_byte && full == 0 && debounce == 0)
-    fifo(tail) = byte_in;
-    tail = tail + 1;
-    if tail == 1025
-        tail = 1;
+    if isempty(fifo)
+        fifo = zeros(1,1024);
     end
-    byte_recieved = 1;
-    debounce = 1;
-end
 
-% Section for calculating num bytes in FIFO
-if (head < tail)
-    bytes_available = (tail - head) - 1;
-else
-    bytes_available = (1024 - head) + tail - 1;
-end
+    full = 0;
+    empty = 0;
 
-dout = byte_out;
+    % handshaking logic
+    byte_received = 0;
+    if store_byte == 0
+        handshake = 0;
+    end
+    if handshake == 1
+        byte_received = 1;
+    end
+    
+    % Section for checking full and empty cases
+    if ((tail == 1 && head == 1024) || ((head + 1) == tail))
+        empty = 1;
+    end
+    if ((head == 1 && tail == 1024) || ((tail + 1) == head))
+        full = 1;
+    end
 
-end
+    %%%%%%%%%%%%%%get%%%%%%%%%%%%%%%%%%%%%
+    if (get_byte && ~empty)
+        head = head + 1;
+        if head == 1025
+            head = 1;
+        end
+        byte_out = fifo(head);
+    end
+    %%%%%%%%%%%%%put%%%%%%%%%%%%%%%%%%%%%
+    if (store_byte && ~full && handshake == 0)
+        fifo(tail) = byte_in;
+        tail = tail + 1;
+        if tail == 1025
+            tail = 1;
+        end
+        byte_received = 1;
+        handshake = 1;
+    end
+
+    % Section for calculating num bytes in FIFO
+    if (head < tail)
+        bytes_available = (tail - head) - 1;
+    else
+        bytes_available = (1024 - head) + tail - 1;
+    end
+
+    dout = byte_out;
